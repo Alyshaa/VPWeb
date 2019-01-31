823e89feb36b518820501763aeee6f2fcd839d00
Scrindor <Scrindor.tv@gmail.com>
2019-01-31 10:08:10 +0100

    replaced " with '

--- editor.php
+++ editor.php
@@ -12,42 +12,42 @@
 $pdo = new PDO('mysql:host=localhost;dbname=vertretungsplan', 'root', '');
 
 echo '<table border = "1">';
-echo "<td><a href='' style=\"text-decoration: none\"> ID </a></td>";
-echo "<td><a href='' style=\"text-decoration: none\"> Stunde </a></td>";
-echo "<td><a href='' style=\"text-decoration: none\"> Klasse </a></td>";
-echo "<td><a href='' style=\"text-decoration: none\"> Vertretung </a></td>";
-echo "<td><a href='' style=\"text-decoration: none\"> Anmerkung <a</td>";
-echo "<td> Hinzugefügt </td>";
+echo '<td><a href="" style="text-decoration: none"> ID </a></td>';
+echo '<td><a href="" style="text-decoration: none"> Stunde </a></td>';
+echo '<d><a href="" style="text-decoration: none"> Klasse </a></td>';
+echo '<td><a href="" style="text-decoration: none"> Vertretung </a></td>';
+echo '<td><a href="" style="text-decoration: none"> Anmerkung <a></td>';
+echo '<td> Hinzugefügt </td>';
 
-$sql = "SELECT * FROM plan ORDER BY id";
+$sql = 'SELECT * FROM plan ORDER BY id';
                         //->fetchALL(PDO::FETCH_ASSOC)
 foreach ($pdo->query($sql)as $row) {
-    echo "<tr>";
+    echo '<tr>';
     echo '<td><a href="" style="text-decoration: none">' . $row['id'] . '</a></td>';
     echo '<td><a href="" style="text-decoration: none">' . $row['stunde'] . '</a></td>';
     echo '<td><a href="" style="text-decoration: none">' . $row['klasse'] . '</a></td>';
     echo '<td><a href="" style="text-decoration: none">' . $row['vertretung'] . '</a></td>';
     echo '<td><a href="" style="text-decoration: none" style="color: black">' . $row['anmerkung'] . '</a></td>';
     echo '<td><a href="" style="text-decoration: none" style="color: black" >' . $row['hinzugefuegt'] . '</a></td>';
-    echo '<td><a href="delete.php?id=' . $row['id'] . '">Entfernen</a></td>';
-    echo "</tr>";
+    echo '<td><a href="delete.php?id="'. $row['id']. '">Entfernen</a></td>';
+    echo '</tr>';
 }
-echo "<td></td>";
-echo "<td></td>";
-echo "<td></td>";
-echo "<td></td>";
-echo "<td></td>";
-echo "<td></td>";
-echo "<td><a href=\"New.php\">Neuer Eintrag</a></td>";
+echo '<td></td>';
+echo '<td></td>';
+echo '<td></td>';
+echo '<td></td>';
+echo '<td></td>';
+echo '<td></td>';
+echo '<td><a href="New.php">Neuer Eintrag</a></td>';
 $fehlende_kollegen = file_get_contents('./docs/fehlendekollegen.txt');
 
 ?>
 
 <a>Fehlende Kollegen</a>
 
-<form method="post">
-    <input type="text" id="fehlendekollegen" value="<?php echo $fehlende_kollegen; ?>">
-    <input type="submit" name="kspeichern" value="speichern" action="?submit">
+<form method='post'>
+    <input type='text' id='fehlendekollegen' value='<?php echo $fehlende_kollegen; ?>'>
+    <input type='submit' name='kspeichern' value='speichern' action='?submit'>
 </form>
 
 <?php
@@ -59,7 +59,7 @@ function kspeichern()
     file_put_contents("./docs/fehlendekollegen.txt",$textfieldvlaue );
     echo $fehlendekollegen;
     echo $textfieldvlaue;
-    echo "Gespeichert";
+    echo 'Gespeichert';
 }
 if(isset($_POST['submit'])){
     kspeichern();
